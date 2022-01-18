208b91acc8ff834ba7e86f142bc2a36d20504b27
Mimmi <omnipotent1021@gmail.com>
2022-01-18 14:36:32 +0900

    add.py

---
+++ app.py
@@ -0,0 +1,15 @@
+
+from flask import Flask  ## flask를 설치해야 빨간 밑줄 삭제됨
+
+app = Flask(__name__)
+@app.route('/')  ## 기본페이지 요청 http://172.20.40.117:8080
+def hell() :
+    return <h1>"hello Flask:D  hohoho"<h1>  ## 클라이언트에 html로 바꿔 응답
+
+@app.route('/test')  ## http://172.20.40.117:8080/test
+def test() :
+    ## 수행할 로직이 들어오는 곳
+    return "test"
+
+if __name__ == '__main__' :
+   app.run("0.0.0.0",port = 8080)
